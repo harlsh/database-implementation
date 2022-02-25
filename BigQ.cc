24c80f07919a9a48cc588b564dcb8608058d60dd
harlsh <harish0796@gmail.com>
2022-02-25 02:12:49 -0500

    refactor

--- BigQ.cc
+++ BigQ.cc
@@ -1,34 +1,30 @@
 #include "BigQ.h"
 
 void* workerMain(void* arg) {
-    WorkerArg* workerArg = (WorkerArg*) arg;
-    priority_queue<Run*, vector<Run*>, RunComparer> runQueue(workerArg->order);
-    priority_queue<Record*, vector<Record*>, RecordComparer> recordQueue (workerArg->order);
+    Payload* payload = (Payload*) arg;
+    priority_queue<Run*, vector<Run*>, RunComparer> runQueue(payload->order);
+    priority_queue<Record*, vector<Record*>, RecordComparer> recordQueue (payload->order);
     vector<Record* > recBuff;
     Record curRecord;
 
-    //Set disk based file for sorting
     File file;
-    char* fileName = "tmp.bin";
+    char* fileName = "storage.bin";
     file.Open(0, fileName);
 
-    //Buffer page used for disk based file
     Page bufferPage;
     int pageIndex = 0;
     int pageCounter = 0;
-    //Retrieve all records from input pipe
-    while (workerArg->in->Remove(&curRecord) == 1) {
+
+    while (payload->in->Remove(&curRecord) == 1) {
         Record* tmpRecord = new Record;
         tmpRecord->Copy(&curRecord);
-        //Add to another page if current page is full
         if (bufferPage.Append(&curRecord) == 0) {
             pageCounter++;
             bufferPage.EmptyItOut();
 
-            //Add to another run if current run is full
-            if (pageCounter == workerArg->runlen) {
+            if (pageCounter == payload->runlen) {
                 recordQueueToRun(recordQueue, runQueue, file, bufferPage, pageIndex);
-                recordQueue = priority_queue<Record*, vector<Record*>, RecordComparer> (workerArg->order);
+                recordQueue = priority_queue<Record*, vector<Record*>, RecordComparer> (payload->order);
                 pageCounter = 0;
             }
 
@@ -37,26 +33,23 @@ void* workerMain(void* arg) {
 
         recordQueue.push(tmpRecord);
     }
-    // Handle the last run
     if (!recordQueue.empty()) {
         recordQueueToRun(recordQueue, runQueue, file, bufferPage, pageIndex);
-        recordQueue = priority_queue<Record*, vector<Record*>, RecordComparer> (workerArg->order);
+        recordQueue = priority_queue<Record*, vector<Record*>, RecordComparer> (payload->order);
     }
-    // Merge for all runs
     while (!runQueue.empty()) {
         Run* run = runQueue.top();
         runQueue.pop();
-        workerArg->out->Insert(run->topRecord);
+        payload->out->Insert(run->topRecord);
         if (run->UpdateTopRecord() == 1) {
             runQueue.push(run);
         }
     }
     file.Close();
-    workerArg->out->ShutDown();
+    payload->out->ShutDown();
     return NULL;
 }
 
-//Used for puting records into a run, which is disk file based
 void* recordQueueToRun(priority_queue<Record*, vector<Record*>, RecordComparer>& recordQueue, 
     priority_queue<Run*, vector<Run*>, RunComparer>& runQueue, File& file, Page& bufferPage, int& pageIndex) {
 
@@ -83,13 +76,12 @@ void* recordQueueToRun(priority_queue<Record*, vector<Record*>, RecordComparer>&
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
     pthread_t worker;
-    //Construct arguement used for worker thread
-    WorkerArg* workerArg = new WorkerArg;
-    workerArg->in = &in;
-    workerArg->out = &out;
-    workerArg->order = &sortorder;
-    workerArg->runlen = runlen;
-    pthread_create(&worker, NULL, workerMain, (void*) workerArg);
+    Payload* payload = new Payload;
+    payload->in = &in;
+    payload->out = &out;
+    payload->order = &sortorder;
+    payload->runlen = runlen;
+    pthread_create(&worker, NULL, workerMain, (void*) payload);
     pthread_join(worker, NULL);
 	out.ShutDown ();
 }
@@ -109,9 +101,7 @@ Run::Run(File* file, int start, int length) {
 }
 
 int Run::UpdateTopRecord() {
-    //if bufferPage is full
     if (bufferPage.GetFirst(topRecord) == 0) {
-        //if reach the last page
         curPage++;
         if (curPage == startPage + runLength) {
             return 0;
@@ -129,9 +119,7 @@ RecordComparer::RecordComparer(OrderMaker* orderMaker) {
 
 bool RecordComparer::operator () (Record* left, Record* right) {
     ComparisonEngine comparisonEngine;
-    if (comparisonEngine.Compare(left, right, order) >= 0)
-        return true;
-    return false;
+    return (comparisonEngine.Compare(left, right, order) >= 0);
 }
 
 RunComparer::RunComparer(OrderMaker* orderMaker) {
@@ -140,9 +128,7 @@ RunComparer::RunComparer(OrderMaker* orderMaker) {
 
 bool RunComparer::operator () (Run* left, Run* right) {
     ComparisonEngine comparisonEngine;
-    if (comparisonEngine.Compare(left->topRecord, right->topRecord, order) >= 0)
-        return true;
-    return false;
+    return (comparisonEngine.Compare(left->topRecord, right->topRecord, order) >= 0);
 }
 
 
--- BigQ.h
+++ BigQ.h
@@ -23,9 +23,7 @@ class Run {
 
 public:
 	Run(File* file, int startPage, int runLength);
-	//Used to update the top record of current run
 	int UpdateTopRecord();
-	//top record of current Run
 	Record *topRecord; 
 
 private: 
@@ -36,7 +34,6 @@ private:
 	int curPage;
 };
 
-//Class used for comparing records for given CNF
 class RecordComparer {
 
 public:
@@ -48,7 +45,6 @@ private:
 
 };
 
-//Class used for comparing run for given CNF
 class RunComparer {
 
 public:
@@ -60,7 +56,6 @@ private:
 
 };
 
-//Struct used as arguement for worker thread
 typedef struct {
 	
 	Pipe *in;
@@ -68,15 +63,10 @@ typedef struct {
 	OrderMaker *order;
 	int runlen;
 	
-} WorkerArg;
+} Payload;
 
-//Main method executed by worker, worker will retrieve records from input pipe, 
-//sort records into runs and puting all runs into priority queue, and geting sorted reecords
-//from priority queue to output pipe
 void* workerMain(void* arg);
 
-//Used for take sequences of pages of records, and construct a run to hold such records, and put run
-//into priority queue
 void* recordQueueToRun(priority_queue<Record*, vector<Record*>, RecordComparer>& recordQueue, 
     priority_queue<Run*, vector<Run*>, RunComparer>& runQueue, File& file, Page& bufferPage, int& pageIndex);
 
